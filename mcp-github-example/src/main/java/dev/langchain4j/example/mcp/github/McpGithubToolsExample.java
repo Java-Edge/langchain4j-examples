d93c9a965b6c89ba4d95d8db2fbd87d766e0e842
JavaEdge <sss2855845850@gmail.com>
2025-12-03 10:38:17 +0800

    ifx:修正编译问题

--- mcp-github-example/src/main/java/dev/langchain4j/example/mcp/github/McpGithubToolsExample.java
+++ mcp-github-example/src/main/java/dev/langchain4j/example/mcp/github/McpGithubToolsExample.java
@@ -1,16 +1,26 @@
 package dev.langchain4j.example.mcp.github;
 
+import dev.langchain4j.http.client.jdk.JdkHttpClient;
+import dev.langchain4j.http.client.jdk.JdkHttpClientBuilder;
 import dev.langchain4j.mcp.McpToolProvider;
 import dev.langchain4j.mcp.client.DefaultMcpClient;
 import dev.langchain4j.mcp.client.McpClient;
 import dev.langchain4j.mcp.client.transport.McpTransport;
 import dev.langchain4j.mcp.client.transport.stdio.StdioMcpTransport;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.request.ResponseFormat;
+import dev.langchain4j.model.localai.LocalAiChatModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.tool.ToolProvider;
+import utils.LangChain4jOllamaContainer;
 
+import java.net.http.HttpClient;
 import java.util.List;
+
+import static dev.langchain4j.internal.Utils.isNullOrEmpty;
+import static utils.AbstractOllamaInfrastructure.*;
 
 public class McpGithubToolsExample {
 
@@ -22,45 +32,61 @@ public class McpGithubToolsExample {
      * <p>
      * Running this example requires Docker to be installed on your machine,
      * because it spawns the GitHub MCP Server as a subprocess via Docker:
-     * `docker run -i mcp/git`.
+     * `docker run -i mcp/github`.
      * <p>
-     * You first need to build the Docker image of the GitHub MCP Server that is available at `mcp/git`.
-     * See https://github.com/modelcontextprotocol/servers/tree/main/src/git to build the image.
+     * You first need to build the Docker image of the GitHub MCP Server that is available at `mcp/github`.
+     * See https://github.com/modelcontextprotocol/servers/tree/main/src/github to build the image.
      * <p>
      * The communication with the GitHub MCP server is done directly via stdin/stdout.
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        ChatModel model = OpenAiChatModel.builder()
-                .apiKey(System.getenv("OPENAI_API_KEY"))
-                .modelName("gpt-4o-mini")
+//        ChatLanguageModel model = OpenAiChatModel.builder()
+//                .apiKey("sk-pRhefgsSUnY0OmPxuelIV4o30wQfZl1Fl1CdLShZqwCJVKtE")
+//                .modelName("gpt-3.5-turbo")
+//                .logRequests(true)
+//                .logResponses(true)
+//                .build();
+
+//        // 连接本地 Ollama 服务
+//        ChatLanguageModel model = OllamaChatModel.builder()
+//                .baseUrl("http://localhost:11434") // Ollama 默认本地服务地址
+//                .modelName("llama3-groq-tool-use:8b") // 你本地 Ollama 拉取的模型名称
+//                .logRequests(true)
+//                .logResponses(true)
+//                .build();
+        // 连接本地 LMStudio 服务
+        HttpClient.Builder httpClientBuilder = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1) ;
+        JdkHttpClientBuilder jdkHttpClientBuilder = JdkHttpClient.builder()
+                .httpClientBuilder(httpClientBuilder);
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("http://127.0.0.1:1234/v1/")
+                .httpClientBuilder(jdkHttpClientBuilder)
+                .modelName("mistralai/ministral-3-14b-reasoning")
                 .logRequests(true)
                 .logResponses(true)
+                .timeout(java.time.Duration.ofSeconds(1200))
                 .build();
 
-        McpTransport transport = new StdioMcpTransport.Builder()
-                .command(List.of("/usr/local/bin/docker", "run", "-e", "GITHUB_PERSONAL_ACCESS_TOKEN", "-i", "mcp/git"))
-                .logEvents(true)
-                .build();
-
-        McpClient mcpClient = new DefaultMcpClient.Builder()
-                .transport(transport)
-                .build();
-
-        ToolProvider toolProvider = McpToolProvider.builder()
-                .mcpClients(List.of(mcpClient))
-                .build();
+//        McpTransport transport = new StdioMcpTransport.Builder()
+//                .command(List.of("/usr/local/bin/docker", "run", "-i", "mcp/github"))
+//                .logEvents(true)
+//                .build();
+//        McpClient mcpClient = new DefaultMcpClient.Builder()
+//                .transport(transport)
+//                .build();
+//
+//        ToolProvider toolProvider = McpToolProvider.builder()
+//                .mcpClients(List.of(mcpClient))
+//                .build();
 
         Bot bot = AiServices.builder(Bot.class)
                 .chatModel(model)
-                .toolProvider(toolProvider)
+//                .toolProvider(toolProvider)
                 .build();
 
-        try {
-            String response = bot.chat("Summarize the last 3 commits of the LangChain4j GitHub repository");
-            System.out.println("RESPONSE: " + response);
-        } finally {
-            mcpClient.close();
-        }
+        String response = bot.chat("Summarize the last 3 commits of the LangChain4j GitHub repository");
+        System.out.println("RESPONSE: " + response);
     }
 }
